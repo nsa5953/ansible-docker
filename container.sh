a7a5a6e36d3225f6986abf38bb495fe79eea70e9
Nilesh Attarde <nsa5953@gmail.com>
2019-12-26 17:44:18 +0530

    Scripts for Docker setup for ansible testing

---
+++ container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKER_CONTAINER_NAME1="ansible-role-test1"
+DOCKER_CONTAINER_NAME2="ansible-role-test2"
+DOCKER_CONTAINER_NAME3="ansible-role-test3"
+SSH_PUBLIC_KEY_FILE=/home/user/.ssh/id_rsa.pub
+SSH_PUBLIC_KEY=$(cat "$SSH_PUBLIC_KEY_FILE")
+
+docker run -dt --privileged=true -p 5001:22 -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name "$DOCKER_CONTAINER_NAME1" -e AUTHORIZED_KEYS="$SSH_PUBLIC_KEY" centos8-systemd:latest
+
+docker run -dt --privileged=true -p 5002:22 -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name "$DOCKER_CONTAINER_NAME2" -e AUTHORIZED_KEYS="$SSH_PUBLIC_KEY" centos8-systemd:latest
+
+docker run -dt --privileged=true -p 5003:22 -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name "$DOCKER_CONTAINER_NAME3" -e AUTHORIZED_KEYS="$SSH_PUBLIC_KEY" centos8-systemd:latest
